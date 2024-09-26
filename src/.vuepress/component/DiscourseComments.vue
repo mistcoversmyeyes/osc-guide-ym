8da7f66fd747bf9c654f05de065e99ba1541d9c5
Samuka007 <samuka007@qq.com>
2024-09-26 15:37:26 +0800

    retry

--- src/.vuepress/component/DiscourseComments.vue
+++ src/.vuepress/component/DiscourseComments.vue
@@ -10,7 +10,7 @@ onMounted(() => {
   window.DiscourseEmbed = {
     discourseUrl: 'https://bbs.scutosc.cn/', 
     discourseEmbedUrl: window.location.origin + useRoute().path.split('#')[0],
-    className: 'body',
+    className: 'vp-page',
   };
   
   (function() {
